536a83f8e586ca087d0102222601451d299be0d5
Moose Davis <moosedavis2011@gmail.com>
2026-06-09 08:14:17 -0400

    feat(interpretations): expose public package surface

---
+++ packages/interpretations/src/index.ts
@@ -0,0 +1,16 @@
+// Public surface of @astro/interpretations. Web + native both import from here.
+export * from "./types.ts";
+export { InterpretationSchema, BankSchema } from "./schema.ts";
+export {
+  signKey, houseKey, transitKey,
+  allSignKeys, allHouseKeys, allTransitKeys,
+  SUBJECTS, TRANSITING_BODIES, TRANSIT_ASPECTS,
+  type TransitAspect,
+} from "./keys.ts";
+export { lookup } from "./lookup.ts";
+export { isTeaser, visibleField, FREE_TEASER_SUBJECTS, type Tier } from "./gating.ts";
+export { transitHits, TRANSIT_DEFS, type TransitHit } from "./transits.ts";
+export {
+  natalRequests, forecastRequests,
+  type NatalRequest, type ForecastRequest,
+} from "./assemble.ts";
